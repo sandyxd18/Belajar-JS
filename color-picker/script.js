bec614af655b24a76c8e246e2954e935ed065504
Sandy <sandyidn04@gmail.com>
2025-07-01 08:55:45 +0700

    project1 done

---
+++ color-picker/script.js
@@ -0,0 +1,10 @@
+document.getElementById("colorInput").addEventListener("input", function(event) {
+    // Get the selected color from the input
+    let selectedColor = event.target.value;
+
+    // Update the color text
+    document.getElementById("colorCode").textContent = selectedColor;
+
+    // Update the background color of the display box
+    document.getElementById("colorDisplay").style.backgroundColor = selectedColor;
+})
